847a1376ccf68237076d26ebdea04efd53fbf693
alioukahere <alioukahere@gmail.com>
2021-03-10 21:27:22 +0000

    authenticate users

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -63,7 +63,7 @@ class User implements UserInterface
      */
     public function getUsername(): string
     {
-        return (string) $this->email;
+        return $this->username;
     }
 
     /**
