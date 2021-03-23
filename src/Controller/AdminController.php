7313447d173de1483c1c03c66bc971fe51dc1980
alioukahere <alioukahere@gmail.com>
2021-03-23 08:37:59 +0000

    admin controller #1

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use App\Repository\ArticleRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * @Route("/admin",)
+ */
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/", name="admin")
+     */
+    public function index(
+        ArticleRepository $articleRepository,
+        UserRepository $userRepository
+    ): Response {
+        $articles = $articleRepository->findBy([], ['updatedAt' => 'DESC']);
+        $users = $userRepository->findAll();
+
+        return $this->render(
+            'admin/index.html.twig',
+            compact($articles, $users)
+        );
+    }
+}
